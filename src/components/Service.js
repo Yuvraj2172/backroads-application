4f144c4d3131e92172639701165ad4da3c5b6d80
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-23 03:28:41 +0530

    Deployment

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { services } from '../data'
 const Service = () => {
-    // const {icon ,title , slogan } = props
-    
   return (
     <div className="section-center services-center">
         {services.map((service)=>{
           const {id , icon , title , slogan} = service;
           return (
-            <article className="service">
+            <article key={id} className="service">
             <span className="service-icon"><i className={icon} ></i></span>
             <div className="service-info">
               <h4 className="service-title">{title}</h4>
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Title from "./Title";
 import Tour from "./Tour";
-import { tour } from "../data";
 const Tours = () => {
   return (
     <section className="section" id="tours">
